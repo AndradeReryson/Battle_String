0f0f4c36df437890c1457d7ccb86aed05a58be05
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2023-04-21 15:55:33 -0300

    Comentei o codigo

--- public/script.js
+++ public/script.js
@@ -31,20 +31,20 @@ var valor_API = "Palavra"; // recebe o valor da API
 function getPalavraAleatoria(){
     return new Promise(function (resolve, reject) {
         $.getJSON('https://random-word-api.herokuapp.com/word', function( data ) {
-            aux = JSON.stringify(data[0]);
-            valor_API = aux.replace(/['"]+/g, '');
-            resolve(valor_API)
+            aux = JSON.stringify(data[0]);      // pega só a palavra, sem os [], mas vem com ""
+            valor_API = aux.replace(/['"]+/g, '');      // tira as ""
+            resolve(valor_API)                          // termina a promise (enquanto nao der resolve o método não pode ser chamado novamente)
         });
     });
 }
 
-// função que roda sem interferir no jogo, e serve para atualizar os valores que vem da API
+// função que roda sem interferir no jogo, e serve para atualizar a palavra que vem da API, sempre chamando uma nova 
 async function updateAPI(){
     await getPalavraAleatoria()
     updateAPI()
 }
 
-// retorna uma valor aleatorio para a coordenada X da palavra
+// retorna um valor aleatorio para a coordenada X da palavra
 function getPosicaoAleatoria(comprimento_palavra){
     
     // calculo fudido ai pra selecionar um valor que não excede o canvas
@@ -66,56 +66,52 @@ class palavra{
     }
 
     init(){    
-        this.list_letras = ['J','i','n','g','l','e','']
+        this.list_letras = ['J','i','n','g','l','e','']     // jingli
         this.text = "Jingle"
         
-        this.setText(valor_API)
+        this.setText(valor_API) // troca os valores da "list_letras" e "text" para a palavra que a API devolveu
         
-        this.height = ctx.font.match(/\d+/).pop() || 10;      
-        //this.size = 50;      
+        this.height = ctx.font.match(/\d+/).pop() || 10; // altura da palavra   
         this.x = getPosicaoAleatoria(this.list_letras.length)
         this.y = 0;      
         this.speedY = 0.9;      
         this.list_acertos = [] // acertos do usuario   
         this.list_erros = [] // erros do usuário
         this.index_atual = 0; // diz qual letra do list_letras será comparada com o input do usuário. 0 = primeira
-        console.log("width-canvas: "+canvas.width)
-        console.log("max: "+(canvas.width + this.list_letras.length*2))
-        console.log("min: "+this.list_letras.length*33)
-        console.log("posibilidades: "+((canvas.width + this.list_letras.length*2) - (this.list_letras.length*33) + 60))
-        console.log('>>> '+this.x+"\n")
-        
     }
 
     update(){
-        this.y += this.speedY;     
+        this.y += this.speedY;    // é chamado em milisegundos, toda vez aumentando a posição y da palavra, fazendo ela descer
     }
 
     conferirInput(input){
         // index_atual controla com qual letra da palavra o input deve ser comparado. Só deve aumentar até o tamanho da palavra - 1
-        if(this.index_atual < this.list_letras.length-1){
+        if(this.index_atual < this.list_letras.length){
 
             // suponhamos que estamos na primeira letra, ou seja, index_atual = 0
             // se a letra inserida no input for igual a letra que está em list_letras[0]
             if(input === this.list_letras[this.index_atual]){
                 this.list_acertos.push(this.index_atual); // o index da letra atual é enviado ao list_acertos
             } else {
-                this.list_erros.push(this.index_atual); // o index da letra atual é enviado ao list_acertos
+                this.list_erros.push(this.index_atual); // o index da letra atual é enviado ao list_erros
             }
 
-            this.index_atual++  // muda a letra atual para o proximo index
+            this.index_atual++  // muda a letra atual de comparação para a proxima
         } else {
-            Palavra_INPUT.value = "";
-            this.init()
+
+            Palavra_INPUT.value = "";   // aqui só roda depois de digitar a palavra inteira
+            this.init() 
         }
     }
 
     draw(){
+        // confere se a letra está em uma certa altura do canvas. 
         if(this.y < canvas.height - 80){         
 
-            
+            // X_origin serve de referencia ao X atual na hora de escrever a primeira letra. O X é aleatório, mas depois de definido nao pode ser mudado
+            // ou então a palavra vai se mover horizontalmente.
+            // No caso, toda vez que o programa desenhar a primeira letra de uma palavra, ele deve desenhar a proxima letra na posição (X + tamanho_da_letra_anterior). Se usar o X diretamente, ele vai mudar o X da palavra inteira, e vai fazer ela se mover na horizontal
             let x_origin = this.x 
-            // centraliza o X no meio da tela com base na qnt de letras da palavra. O valor 11 é manual
 
             for(let i = 0; i < this.list_letras.length; i++){
                 
@@ -126,55 +122,65 @@ class palavra{
                     // desenha o retangulo por tras das letras
                     ctx.fillRect(x_origin, this.y - 30, ctx.measureText(this.list_letras[i]).width, 30)
                 } else if (this.list_erros.includes(i)){
+                    // esse else é caso não esteja na lista de acertos
                     ctx.beginPath()
                     ctx.fillStyle = 'red'
                     ctx.fillRect(x_origin, this.y - 30, ctx.measureText(this.list_letras[i]).width, 30)
                 }
 
+                // desenha cada letra da palavra
                 ctx.beginPath()
                 ctx.font = 'bold 35px Arial'
                 ctx.fillStyle = 'black';
                 ctx.textBaseLine = 'top'
                 ctx.fillText(this.list_letras[i], x_origin, this.y)
-                x_origin += ctx.measureText(this.list_letras[i]).width // atualiza o X para o valor atual + o tamanho da letra que foi escrita, assim a proxima letra a ser escrita ficará do lado dela
+                x_origin += ctx.measureText(this.list_letras[i]).width // atualiza o X_origin para o valor atual + o tamanho da letra que foi escrita, assim a proxima letra a ser escrita ficará do lado dela
                 
             }
             
         } else {
-            this.init(); // reseta a palavra
+            this.init(); // reseta a palavra, voltando ela pra Y = 0, e trocando por uma palavra nova vinda da API
         }
     }
 
     setText(palavra){
-        console.log("setText to "+palavra)
+        //console.log("setText to "+palavra)
+
         this.text = palavra
         this.height = ctx.font.match(/\d+/).pop() || 10
         
-        this.list_letras.length = 0;
+        this.list_letras.length = 0; // limpa o vetor das letras
+
         for(let i = 0; i < palavra.length; i++){
-            this.list_letras.push(palavra[i]);
+            this.list_letras.push(palavra[i]);  // coloca letra por letra da palavra nova
         }
 
-        console.log(this.list_letras)
+        console.log(this.list_letras) 
         
     }
     
     
 }
  
-let palavra1; // palavra
+let palavra1; // palavra1 será a palavra que desce, ainda não foi criado o objeto
 
+// essa função é a que faz a palavra descer, toda vez que é chamada, ela muda o Y da palavra para +1 e desenha ela de novo, bem rapido, o que da ilusão de movimento
 function manipularPalavra(){
     palavra1.update();
     palavra1.draw();
 }
 
+// uma vez desenhada a palavra no canvas, ela permanece la, mesmo desenhando outra. Para apagar a posição antiga, usamos o clearRect, e chamamos o metodo que desenha
 function animate(){
     ctx.clearRect(0, 0, canvas.width, canvas.height);   
     manipularPalavra();            
-    requestAnimationFrame(animate);         
+    requestAnimationFrame(animate);     // torna a função recursiva, ou seja, sempre que a palavra for desenhada, 
 }
 
+// primeira função a ser executada, ela que inicia todas as outras
+// primeiro, ela inicia o chamado da API por uma nova palavra, antes de desenhar qualquer palavra. A API demora um pouco pra devolver o valor, por isso rodamos antes
+// depois, usamos o await new promise [...] para fazer a função esperar 5 segundos (5000 milisegundos) antes de criar o objeto da palavra e desenhá-la
+// o async permite que usemos o await na função. Isso faz com que a função espere a linha terminar sua execução antes de ir pra próxima
 async function iniciar() {
     updateAPI();
     await new Promise(resolve => setTimeout(resolve, 5000));
@@ -182,8 +188,10 @@ async function iniciar() {
     animate(); 
 }     
 
-iniciar()
+iniciar() 
 
+// previne a inserção da tecla Enter (keycode 13) e barra de espaço (keycode 32)
+// http://www.javascripter.net/faq/keycodes.htm
 Palavra_INPUT.addEventListener('keypress', (evt) => {
     if(evt.keyCode === 13 || evt.keyCode === 32){
         evt.preventDefault()
@@ -192,6 +200,7 @@ Palavra_INPUT.addEventListener('keypress', (evt) => {
     }
 })
 
+// previne inserção de backspace, keycode 8
 Palavra_INPUT.addEventListener('keydown', (evt) => {
     if(evt.keyCode === 8 ){
         evt.preventDefault()
