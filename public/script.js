7677efa8a4da8925a27228ead6296a88c81ba576
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2023-04-21 22:50:07 -0300

    iniciar, cronometro, contagem

--- public/script.js
+++ public/script.js
@@ -2,12 +2,14 @@
 /*const socket = io();*/
 //const CIT_ALEATORIA_API =  'http://api.quotable.io/random';
 // recebe as palavras da API
+// document.fonts.add();
 const CIT_ALEATORIA_API = 'https://random-word-api.herokuapp.com/word';
 const Palavra_TELA = document.querySelector('.palavra-display');
 const Palavra_INPUT = document.querySelector('.palavra-input');
 const Tempo_TELA = document.getElementById('timer');
 const Botao_iniciar = document.getElementById('Iniciar');
 const Palavra_bomb = document.querySelector('.pilha-palavras');
+var JOGO_EM_CURSO = false // vai dizer se o jogo está em curso ou se acabou
 
 const canvas = document.getElementById('palavra_display');
 const ctx = canvas.getContext('2d') // o ctx é quem desenha as formas
@@ -25,6 +27,8 @@ window.addEventListener('resize', function(){
 });
 
 
+desligarInput(true)
+
 var valor_API = "Palavra"; // recebe o valor da API
 
 // função que faz o chamado pra API e retorna uma palavra aleatória
@@ -40,15 +44,21 @@ function getPalavraAleatoria(){
 
 // função que roda sem interferir no jogo, e serve para atualizar a palavra que vem da API, sempre chamando uma nova 
 async function updateAPI(){
-    await getPalavraAleatoria()
-    updateAPI()
+    if(JOGO_EM_CURSO){
+        await getPalavraAleatoria()
+        await esperarSegundos(0.5)
+        console.log(valor_API)
+        updateAPI()
+    } else {
+        return
+    }
 }
 
 // retorna um valor aleatorio para a coordenada X da palavra
 function getPosicaoAleatoria(comprimento_palavra){
     
     // calculo fudido ai pra selecionar um valor que não excede o canvas
-    let calc = Math.abs(Math.random() * (canvas.width + comprimento_palavra*2) - (comprimento_palavra*33) + 40)
+    let calc = Math.abs(Math.random() * (canvas.width + comprimento_palavra*2) - (comprimento_palavra*25))
 
     // caso o valor seja < 100 ele soma + 100. Isso para evitar que a palavra fique na extremidade esquerda da tela
     if(calc < 100){
@@ -66,6 +76,7 @@ class palavra{
     }
 
     init(){    
+        desligarInput(false)
         this.list_letras = ['J','i','n','g','l','e','']     // jingli
         this.text = "Jingle"
         
@@ -84,11 +95,12 @@ class palavra{
         this.y += this.speedY;    // é chamado em milisegundos, toda vez aumentando a posição y da palavra, fazendo ela descer
     }
 
-    conferirInput(input){
-        // index_atual controla com qual letra da palavra o input deve ser comparado. Só deve aumentar até o tamanho da palavra - 1
+    async conferirInput(input){
+        // index_atual controla com qual letra da palavra o input deve ser comparado. Deve rodar enquanto for menor que a palavra
+
         if(this.index_atual < this.list_letras.length){
 
-            // suponhamos que estamos na primeira letra, ou seja, index_atual = 0
+            // suponhamos que estamos na primeira letra , ou seja, index_atual = 0
             // se a letra inserida no input for igual a letra que está em list_letras[0]
             if(input === this.list_letras[this.index_atual]){
                 this.list_acertos.push(this.index_atual); // o index da letra atual é enviado ao list_acertos
@@ -96,10 +108,19 @@ class palavra{
                 this.list_erros.push(this.index_atual); // o index da letra atual é enviado ao list_erros
             }
 
-            this.index_atual++  // muda a letra atual de comparação para a proxima
+            this.index_atual++
+            
+            // quando o index_atual == tamanho da palavra, ele não faz nada, pois para entrar no else e chamar a proxima palavra, o if precisa rodar + uma vez. isso força o usuário a digitar algo no input mesmo depois de ter digitado todas as letras.
+            // para resolver, depois de incrementar, se o index_atual for igual o tamanho da palavra, o if abaixo aciona um keypress no input, o que faz o if rodar mais uma vez e cair no else
+            if(this.index_atual == this.list_letras.length){
+                Palavra_INPUT.dispatchEvent(new Event('keypress'));     // aciona um keypress no input
+            }
         } else {
-
+            await esperarSegundos(0.05)
+            desligarInput(true);
+            await esperarSegundos(0.5)
             Palavra_INPUT.value = "";   // aqui só roda depois de digitar a palavra inteira
+            
             this.init() 
         }
     }
@@ -139,6 +160,7 @@ class palavra{
             }
             
         } else {
+            Palavra_INPUT.value = ""
             this.init(); // reseta a palavra, voltando ela pra Y = 0, e trocando por uma palavra nova vinda da API
         }
     }
@@ -155,14 +177,47 @@ class palavra{
             this.list_letras.push(palavra[i]);  // coloca letra por letra da palavra nova
         }
 
-        console.log(this.list_letras) 
+        //console.log(this.list_letras) 
         
     }
     
     
 }
- 
+
+// imrpime a contagem regressiva e segura o program pela quantidade de segundos determina
+async function contagemRegress(tempo){
+    ctx.beginPath()
+    ctx.font = 'bold 90px Arial'
+    ctx.fillStyle = 'black';
+    ctx.textBaseLine = 'top'
+    
+    let pos_x = canvas.width/2 - 45
+    let pos_y = canvas.height/2 - 10
+    
+
+    for(let i = tempo; i > 0; i--){
+        ctx.clearRect(0, 0, canvas.width, canvas.height); 
+        ctx.fillText(i, pos_x, pos_y);
+        await esperarSegundos(1);
+    }
+}
+
 let palavra1; // palavra1 será a palavra que desce, ainda não foi criado o objeto
+
+// primeira função a ser executada, ela que inicia todas as outras
+// primeiro, ela inicia o chamado da API por uma nova palavra, antes de desenhar qualquer palavra. A API demora um pouco pra devolver o valor, por isso rodamos antes
+// depois, esperaramos 5 segundos (5000 milisegundos) antes de criar o objeto da palavra e desenhá-la
+// o async permite que usemos o await na função. Isso faz com que a função espere a linha terminar sua execução antes de ir pra próxima
+async function iniciar() {
+    JOGO_EM_CURSO = true;
+    updateAPI();
+    await contagemRegress(5);
+    palavra1 = new palavra();  
+    desligarInput(false)
+    tempo_inicial = 10
+    startTimer();
+    animate(); 
+}     
 
 // essa função é a que faz a palavra descer, toda vez que é chamada, ela muda o Y da palavra para +1 e desenha ela de novo, bem rapido, o que da ilusão de movimento
 function manipularPalavra(){
@@ -170,25 +225,17 @@ function manipularPalavra(){
     palavra1.draw();
 }
 
-// uma vez desenhada a palavra no canvas, ela permanece la, mesmo desenhando outra. Para apagar a posição antiga, usamos o clearRect, e chamamos o metodo que desenha
+// uma vez desenhada a palavra no canvas, ela permanece la, mesmo desenhando outra. Para apagar a posição antiga, usamos o clearRect, e chamamos o metodo manipularPalavra().
 function animate(){
-    ctx.clearRect(0, 0, canvas.width, canvas.height);   
-    manipularPalavra();            
-    requestAnimationFrame(animate);     // torna a função recursiva, ou seja, sempre que a palavra for desenhada, 
+    if(JOGO_EM_CURSO){
+        ctx.clearRect(0, 0, canvas.width, canvas.height);   
+        manipularPalavra();            
+        requestAnimationFrame(animate);     // torna a função recursiva, ou seja, sempre que a palavra for desenhada, ja vai ser redesenhada
+    } else {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);   
+        return 
+    }
 }
-
-// primeira função a ser executada, ela que inicia todas as outras
-// primeiro, ela inicia o chamado da API por uma nova palavra, antes de desenhar qualquer palavra. A API demora um pouco pra devolver o valor, por isso rodamos antes
-// depois, usamos o await new promise [...] para fazer a função esperar 5 segundos (5000 milisegundos) antes de criar o objeto da palavra e desenhá-la
-// o async permite que usemos o await na função. Isso faz com que a função espere a linha terminar sua execução antes de ir pra próxima
-async function iniciar() {
-    updateAPI();
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    palavra1 = new palavra();  
-    animate(); 
-}     
-
-iniciar() 
 
 // previne a inserção da tecla Enter (keycode 13) e barra de espaço (keycode 32)
 // http://www.javascripter.net/faq/keycodes.htm
@@ -206,3 +253,60 @@ Palavra_INPUT.addEventListener('keydown', (evt) => {
         evt.preventDefault()
     } 
 })
+
+// funcao que desliga/liga o input
+function desligarInput(b){
+    if (b == true){
+        Palavra_INPUT.disabled = true
+    } else {
+        Palavra_INPUT.disabled = false;
+    } 
+    Palavra_INPUT.focus();
+}
+
+// função que recebe segundos e faz o javascript esperar através de uma promise
+// serve apenas para atrasar algumas outras funções
+async function esperarSegundos(segundos){
+    tempo = segundos * 1000
+    await new Promise(resolve => setTimeout(resolve, tempo));
+}
+
+
+/*
+    Colocar em outro arquivo depois
+*/
+
+var tempo_inicial; // setado dentro do click do botão_iniciar
+var intervalo_timer; // variavel com o id do intervalo que controla o cronometro
+
+function startTimer() {
+  intervalo_timer = setInterval(() => {
+    timer.innerText = cronometro()
+  }, 1000)
+}
+
+function cronometro() {
+  if(tempo_inicial>0){  //  se o tempo for maior de 0 ele atualiza o cronometro
+    var crono = tempo_inicial--
+    return crono
+  } else if(tempo_inicial == 0){
+    finalizarPartida()
+    return 0 // 
+  }
+}
+
+Botao_iniciar.addEventListener('click', () => {
+    iniciar();
+    Botao_iniciar.hidden = true;
+})
+
+async function finalizarPartida(){
+    JOGO_EM_CURSO = false
+    palavra1.init();
+    Botao_iniciar.hidden = false // mostra o botao iniciar denovo
+    desligarInput(true); // desliga o textfield
+    Palavra_INPUT.value = ""
+    timer.innerText = 0;           // zera o timer
+    tempo_inicial = -1; // setado para -1 para nao criar um laço infinito no cronometro
+    clearInterval(intervalo_timer);
+  }
